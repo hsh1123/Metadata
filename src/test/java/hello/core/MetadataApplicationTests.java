6b90c41c23c00fee7164c067341024bad729a712
hsh1123 <sean112333@gmail.com>
2023-03-08 00:43:22 +0900

    first commit

---
+++ src/test/java/hello/core/MetadataApplicationTests.java
@@ -0,0 +1,29 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class MetadataApplicationTests {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join(){
+        //given
+        Member member = new Member(1L,"memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        assertThat(member).isEqualTo(findMember);
+    }
+
+}
