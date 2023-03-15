1794780fb14b3aeb5c086ad38bd7bde9ba0737fd
hsh1123 <sean112333@gmail.com>
2023-03-15 21:12:29 +0900

    OCP , DIP 원칙 준수하게 리펙터링 (add AppConfig)

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 
-class RateDiscountPolicyTest {
+public class RateDiscountPolicyTest {
 
     RateDiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
 
