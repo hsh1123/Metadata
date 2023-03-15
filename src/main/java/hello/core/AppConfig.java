771691362f319775062ad0eaeb1265beac4f5085
hsh1123 <sean112333@gmail.com>
2023-03-15 22:49:48 +0900

    Spring Container를 이용하여 객체 의존성 관리

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -10,22 +10,25 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
-
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(MemberRepository());
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(MemberRepository(), discountPolicy());
     }
-
-    private MemberRepository MemberRepository() {
+    @Bean
+    public MemberRepository MemberRepository() {
         return new MemoryMemberRepository();
     }
-
-    private DiscountPolicy discountPolicy(){
+    @Bean
+    public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
